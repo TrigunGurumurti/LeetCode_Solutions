3ef496558bb329c36d455bdd5da14dcaa53b12c1
TrigunGurumurti <triguntamragouri@gmail.com>
2024-11-17 15:11:24 +0530

    Time: 0 ms (100%), Space: 8 MB (84.45%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool list[58];
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+        for(int i = 0; i < jewels.size(); i++)
+            list[jewels[i] - 65] = true;
+        for(int i = 0; i < stones.size(); i++)
+        {
+            if(list[stones[i] - 65])
+                count++;
+        }        
+        return count;
+    }
+};
