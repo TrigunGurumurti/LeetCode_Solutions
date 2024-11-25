d4d31bf43aa1ee9aeba46c3b35acd325360a219d
TrigunGurumurti <triguntamragouri@gmail.com>
2024-11-25 21:30:31 +0530

    Time: 0 ms (100%), Space: 9.4 MB (43.86%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> result(n+1, 0);
+        for(int i = 0; i <= n; i++)
+            result[i] = result[i/2] + (i&1);
+        return result;
+    }
+};
