27d1031190503af699bd8d946305f7026c2570e7
TrigunGurumurti <triguntamragouri@gmail.com>
2024-12-02 14:45:58 +0530

    Time: 0 ms (100%), Space: 8.1 MB (7.8%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        stringstream s(sentence);
+        string word;
+        int index = 1;
+        while (s >> word)
+        {
+            if(word.find(searchWord) == 0)
+            {
+                return index;
+            }
+            index++;
+        }
+        return -1; 
+    }
+};
