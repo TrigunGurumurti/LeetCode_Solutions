1ad33b726b263ed36f876c008f8de7555dc8bd10
TrigunGurumurti <triguntamragouri@gmail.com>
2024-12-29 21:10:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int start = 1;
+        int end = num; 
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if(mid * mid == num)
+                return 1;
+            else if(mid * mid < num)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return 0;
+    }
+};
